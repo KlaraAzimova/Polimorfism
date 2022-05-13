e756be9bff8c8c2cac2c5d19c089e0a5b073da72
Klara <azimovaklara18@gmail.com>
2022-05-13 15:41:54 +0000

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        Horse horse = new Horse("Boika", 4, "black");
-//        System.out.println(horse);
-//        horse.eat();
-//        horse.speed(60,40);
-//
-//        Animal dog = new Dog("Alabay", 3, "white");
-//        System.out.println(dog);
-//        dog.eat();
-//        dog.speed(40,50,60);
+
         Animal shark = new Shark("Shark", 5, "blue");
         Animal eagle = new Eagle("Eagle", 6, "black");
         Animal turtle = new Turtle("Turtle", 12, "brown");
